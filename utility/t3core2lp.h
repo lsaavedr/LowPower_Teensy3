4020bc294a062a3dc6c6c6414767c73c2ff7fbaf
duff2013 <duff@mflin.lan>
2014-02-17 12:02:20 -0500

    still trying to find algorithm for microsecond delay in all CPU freq

--- utility/t3core2lp.h
+++ utility/t3core2lp.h
@@ -12,7 +12,9 @@
 extern "C" {
 #endif
     uint32_t micros_lp(uint32_t f_cpu);
+    
     void delay_lp(uint32_t msec, uint32_t f_cpu);
+    
     static inline uint32_t millis_lp(void) __attribute__((always_inline, unused));
     static inline uint32_t millis_lp(void) {
         volatile uint32_t ret = systick_millis_count; // single aligned 32 bit is atomic;
@@ -21,10 +23,9 @@ extern "C" {
     
     static inline void delayMicroseconds_lp(uint32_t, uint32_t) __attribute__((always_inline, unused));
     static inline void delayMicroseconds_lp(uint32_t usec, uint32_t f_cpu) {
-        
-        long n;
-        float cpu = f_cpu;
-        n = (usec * (cpu/3000000));//-32856;
+        //long n;
+        //float cpu = f_cpu;
+        //n = (usec * (cpu/3000000));//-32856;
         /*if (usec == 0) return;
         else if (f_cpu >= 24000000) {
             n = ((usec) * (f_cpu/3000000)) - 16;
@@ -38,7 +39,7 @@ extern "C" {
                      "L_%=_delayMicroseconds_lp:"       "\n\t"
                      "subs   %0, #1"                    "\n\t"
                      "bge    L_%=_delayMicroseconds_lp"	"\n"
-                     : "+r" (n) :
+                     : "+r" (usec) :
                      );
         
     }
