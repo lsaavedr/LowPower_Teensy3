2d5df0b41f4294d00b9e1219cca6d223d91a8bec
duff2013 <cmduffy@engr.psu.edu>
2014-03-19 13:26:27 -0400

    Using bit band to set registers

--- utility/bitband.h
+++ utility/bitband.h
@@ -8,7 +8,7 @@
 #define __BITBAND_H__
 
 #define BITBAND_ADDR(reg, bit) (((uint32_t)&(reg) - 0x40000000) * 32 + (bit) * 4 + 0x42000000)
-#define BITBAND_U32(reg, bit) (*(uint32_t *)BITBAND_ADDR((reg), (bit)))
-#define BITBAND_U8(reg, bit) (*(uint8_t *)BITBAND_ADDR((reg), (bit)))
+#define BITBAND_U32(reg, bit) (*(volatile uint32_t *)BITBAND_ADDR((reg), (bit)))
+#define BITBAND_U8(reg, bit) (*(volatile uint8_t *)BITBAND_ADDR((reg), (bit)))
 
 #endif
