14cd3418447c343e7ca461b880e8f48aec1e6f11
duff2013 <cmduffy@engr.psu.edu>
2014-03-19 13:29:30 -0400

    Using bitband to set registers

--- utility/rtc.c
+++ utility/rtc.c
@@ -7,12 +7,17 @@
  *******************************************************************************/
 
 #include "rtc.h"
+/*#include "bitband.h"
 
-void rtc_alarm(unsigned long sec) {
-    if (!(RTC_IER & RTC_IER_TAIE_MASK)) RTC_IER |= RTC_IER_TAIE_MASK;
+#define RTC_IER_TAIE_BIT   0x02
+
+void rtc_alarm(unsigned long sec){
+    if (!(RTC_IER & RTC_IER_TAIE_MASK)) BITBAND_U32(RTC_IER, RTC_IER_TAIE_BIT) = 1;
     RTC_TAR = rtc_get() + (sec - 1);
 }
 
 void rtc_stop(void) {
-    RTC_IER &= ~RTC_IER_TAIE_MASK;
+    BITBAND_U32(RTC_IER, RTC_IER_TAIE_BIT) = 0;
+    //RTC_IER &= ~RTC_IER_TAIE_MASK;
 }
+*/
--- utility/rtc.h
+++ utility/rtc.h
@@ -9,14 +9,26 @@
 /********************************************************************/
 #include "mk20dx128.h"
 #include "mk20dx128_ext.h"
-//#include "Arduino.h"
+#include "bitband.h"
+
+#define RTC_IER_TAIE_BIT   0x02
 /********************************************************************/
 #ifdef __cplusplus
 extern "C" {
 #endif
     // function prototypes
-    void rtc_alarm(unsigned long sec);
-    void rtc_stop(void);
+    static inline void rtc_alarm(unsigned long) __attribute__((always_inline, unused));
+    static inline void rtc_alarm(unsigned long sec) {
+        if (!BITBAND_U32(RTC_IER, RTC_IER_TAIE_BIT)) BITBAND_U32(RTC_IER, RTC_IER_TAIE_BIT) = 1;
+        //if (!(RTC_IER & RTC_IER_TAIE_MASK)) BITBAND_U32(RTC_IER, RTC_IER_TAIE_BIT) = 1;
+        RTC_TAR = rtc_get() + (sec - 1);
+    }
+    
+    static inline void rtc_stop(void) __attribute__((always_inline, unused));
+    static inline void rtc_stop(void) {
+        BITBAND_U32(RTC_IER, RTC_IER_TAIE_BIT) = 0;
+        //RTC_IER &= ~RTC_IER_TAIE_MASK;
+    }
 #ifdef __cplusplus
 }
 #endif
